e1671508335b66be1b1ed333de0b34bc2610cd2e
ThomasAmiand <amiand.thomas@gmail.com>
2015-04-21 11:42:27 +0200

    DAO user réalisée

--- TwitterWithRedis/src/daoImpl/UserDAOImpl.java
+++ TwitterWithRedis/src/daoImpl/UserDAOImpl.java
@@ -1,6 +1,12 @@
 package daoImpl;
 
+import beans.User;
 import dao.UserDAO;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by tamiand on 21/04/2015.
@@ -9,25 +15,46 @@ public class UserDAOImpl implements UserDAO {
 
     ClientRedis client = new ClientRedis();
 
+    static final String keyUsers = "users";
+    static final String key = "user:";
+    static final String loginField = "name";
+    static final String passField = "password";
 
 
     @Override
     public void createUser(String login, String password) {
 
+        String UID = String.valueOf(new Date().getTime());
+
+        Map<String,String> userUID = new HashMap<String, String>();
+        userUID.put(login, UID);
+
+        client.getClient().hmset(keyUsers, userUID);
+
+        Map<String,String> userProperties = new HashMap<String,String>();
+        userProperties.put(loginField, login);
+        userProperties.put(passField, password);
+
+        client.setHM(key + UID, userProperties);
     }
 
     @Override
     public void deleteUser(String login) {
 
+        List<String> uids = client.getClient().hmget(keyUsers, login);
+        String uidToDelete = uids.get(0);
+        client.zRem(login);
+
+        client.delete(key+uidToDelete);
     }
 
     @Override
-    public void getUsers() {
+    public User getUser(String login) {
 
-    }
-
-    @Override
-    public void getUser(String login) {
-
+        List<String> uids = client.getClient().hmget(keyUsers, login);
+        String uidToGet = uids.get(0);
+        List<String> userProperties =  client.getClient().hmget(key+uidToGet, loginField, passField);
+        User toReturn = new User(userProperties.get(0), userProperties.get(1));
+        return toReturn;
     }
 }
